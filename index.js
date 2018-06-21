96c32286bcb465c330b5be3efcdd647c5f6bcf40
juwon24luv123 <juwon24luv1@yahoo.com>
2018-06-21 10:11:40 -0700

    change greetings message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'hello'});
+    res.send({hi: 'hello express'});
 });
 
 const PORT = process.env.PORT || 5000;
