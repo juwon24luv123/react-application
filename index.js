959f4fb6a09899c5712fc84ae4c42734281a0ea0
juwon24luv123 <juwon24luv1@yahoo.com>
2018-06-21 09:39:39 -0700

    Another deploy

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'hello'})
+    res.send({hi: 'hello'});
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
-})
+});
