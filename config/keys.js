15893fb93725af1ba83f8e63327e6aafa76c0a3b
juwon24luv123 <juwon24luv1@yahoo.com>
2018-06-26 07:18:11 -0700

    finished authentication flows

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 
 if (process.env.NODE_ENV === 'production') {
     //we are in production - return production
-    module.export = require('./prod');
+    module.exports = require('./prod');
 } else {
     // we are in development return dev
     module.exports = require('./dev');
